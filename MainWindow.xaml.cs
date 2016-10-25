318e345360eeeb47b639bcbf9803821771539650
drzzm32 <drzzm32@gmail.com>
2016-10-25 18:18:07 +0800

    init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows;
+
+
+namespace LightCraft
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : MahApps.Metro.Controls.MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
